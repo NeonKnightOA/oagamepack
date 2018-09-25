cf5da18634a2a6bf737360d351533f41ad9bf705
sago007 <poul@poulsander.com>
2018-09-25 19:30:19 +0200

    Updated script to also update oa.game

--- scripts/create_output.bat
+++ scripts/create_output.bat
@@ -1,3 +1,5 @@
 rem This file is designed to be executed from a python3 version of the Anaconda terminal
 python create_ent.py > ../install/OAPack/install/baseoa/entities.ent
 python create_def.py > ../install/OAPack/install/baseoa/entities.def
+python create_ent.py > ../oa.game/baseoa/entities.ent
+python create_def.py > ../oa.game/baseoa/entities.def
